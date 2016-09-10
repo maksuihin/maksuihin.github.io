e740d365e3576e2ddcc1ea84471ba6b25b619a74
maksuihin <maksuihin@gmail.com>
2016-09-10 16:26:55 +0800

    Create 0912.py

---
+++ dictionary/0912.py
@@ -0,0 +1,10 @@
+myDictionary = {
+  'computer-generated intermediaries': {
+    'definition': r"Also called tweening or betweening. The computer generates the forms in between collected data, for example, predicting the gradient and heights between the recorded contour lines.",
+    'sources': [
+      r"Chrisman, N. R. (2006). Charting the Unknown: How Computer Mapping at Harvard Became GIS. Tennessee: Esri Press. p. 45.",
+      ],
+    'date_entered': "2016-09-10",
+    'presented': False,
+   },
+}
